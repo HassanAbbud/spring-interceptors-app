564551ec1d7f8780b9e8b581df394d8fc0358f58
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-12 10:34:57 -0600

    first commit

---
+++ src/main/java/com/hassan/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -0,0 +1,22 @@
+package com.hassan.springboot.app.interceptor.springboot_interceptor.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoadingTimeInterceptor implements HandlerInterceptor{
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+       return true;
+    }
+    
+}
